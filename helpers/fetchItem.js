fe15142cd328b2bc9353981919e754558429ae1f
Juliano Boese <juliano.boese@gmail.com>
2022-01-15 17:51:23 -0300

    Refatora fetchProducts e fetchItem

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,4 +1,8 @@
 const fetchItem = async (itemSku) => {
+  if (!itemSku) {
+    throw new Error('You must provide an url');
+  }
+
   const response = await fetch(`https://api.mercadolibre.com/items/${itemSku}`);
   const data = await response.json();
   return data;
--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,9 @@
-const fetchProducts = async (search) => {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
+const fetchProducts = async (query) => {
+  if (!query) {
+    throw new Error('You must provide an url');
+  }
+
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
   const data = await response.json();
   return data;
 };
